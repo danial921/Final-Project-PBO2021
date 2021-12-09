ca2fd84978564454f18dd127d62699b451d04f54
HelsaNestaD <70515589+helsanesta@users.noreply.github.com>
2021-12-09 07:59:28 +0700

    Update UnitSize

--- src/snakegame/com/GamePanel.java
+++ src/snakegame/com/GamePanel.java
@@ -9,7 +9,7 @@ public class GamePanel extends JPanel implements ActionListener{
 
 	static final int SCREEN_WIDTH = 1300;
 	static final int SCREEN_HEIGHT = 750;
-	static final int UNIT_SIZE = 50;
+	static final int UNIT_SIZE = 25;
 	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/(UNIT_SIZE*UNIT_SIZE);
 	static final int DELAY = 175;
 	final int x[] = new int[GAME_UNITS];
