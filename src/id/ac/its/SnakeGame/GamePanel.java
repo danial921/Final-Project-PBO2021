d407cc2594a8e4397d47f5da85bc4fbcc7648cd6
DanialFarros <72653810+danial921@users.noreply.github.com>
2021-12-09 08:21:40 +0700

    Update GamePanel.java

--- src/id/ac/its/SnakeGame/GamePanel.java
+++ src/id/ac/its/SnakeGame/GamePanel.java
@@ -14,7 +14,7 @@ public class GamePanel extends JPanel implements ActionListener {
 
 	static final int SCREEN_WIDTH = 1300;
 	static final int SCREEN_HEIGHT = 750;
-	static final int UNIT_SIZE = 50;
+	static final int UNIT_SIZE = 25;
 	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/(UNIT_SIZE*UNIT_SIZE);
 	static final int DELAY = 120;
 	final int x[] = new int[GAME_UNITS];
