5a6064a8fe8676e944a2786430a3922a73e50b81
HelsaNestaD <70515589+helsanesta@users.noreply.github.com>
2021-12-15 14:03:09 +0700

    Update GameFrame.java

--- src/id/ac/its/SnakeGame/GameFrame.java
+++ src/id/ac/its/SnakeGame/GameFrame.java
@@ -6,7 +6,7 @@ public class GameFrame extends JFrame {
 	GameFrame(int Delay){
 		this.add(new GamePanel(Delay));
 		this.setTitle("Snake");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
 		this.pack();
 		this.setVisible(true);
